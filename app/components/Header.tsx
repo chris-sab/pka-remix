4d24f2f8d1b32b3ccdde7287ab4efb58fd4f6cbb
Christopher Saburo <christopher.sab@outlook.com>
2024-07-25 02:48:47 +0900

    finished header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,31 @@
+import { Link } from "@remix-run/react";
+
 export const Header = () => {
   return (
-    <div>Header</div>
+    <section className="w-full flex justify-center bg-indigo-950">
+      <div className="w-full max-w-full lg:max-w-[1024px] xl:max-w-[1280px] 2xl:max-w-[1440px]">
+        <div className="flex justify-between h-16">
+          <div className="flex flex-row gap-12">
+            <Link to="/" className="text-white font-bold text-3xl my-auto">
+              PKA
+            </Link>
+
+            <div className="flex flex-row gap-8 my-auto">
+              <Link to="/" className="text-white font-medium text-base uppercase">
+                Home
+              </Link>
+
+              <Link to="/profile" className="text-white font-medium text-base uppercase">
+                Profile
+              </Link>
+            </div>
+          </div>
+
+          <Link to="/" className="text-white font-medium text-base my-auto uppercase">
+            Sign In
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
--- app/pages/ProfilePage.tsx
+++ app/pages/ProfilePage.tsx
@@ -2,7 +2,19 @@ export const ProfilePage = () => {
   return (
     <main className="flex flex-col items-center justify-between min-h-[100vh] bg-white">
       <section className="w-full max-w-full lg:max-w-[1024px] xl:max-w-[1280px] 2xl:max-w-[1440px] px-5 xl:px-0">
-        Profile Page
+        <div className="flex flex-col">
+          <div className="flex flex-row gap-4 my-auto">
+            <img
+              className="rounded-full h-[100px]"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt="user"
+            />
+
+            <span className="my-auto text-indigo-950 font-bold text-lg">
+              Christopher Saburo
+            </span>
+          </div>
+        </div>
       </section>
     </main>
   );
