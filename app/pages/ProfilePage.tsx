cce064e74aa0c6022749c97183bfdd32e9d3da7f
Christopher Saburo <christopher.sab@outlook.com>
2024-07-25 11:44:18 +0900

    updated form module

--- app/pages/ProfilePage.tsx
+++ app/pages/ProfilePage.tsx
@@ -1,8 +1,11 @@
-import { Form } from "@remix-run/react";
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { actions as profileActions } from "../store/profile/slice";
+
 import { ActivityCard } from "~/components/Cards";
 import { getProfile } from "~/store/profile/selector";
 import { getBalance } from "~/store/balance/selector";
+import { ProfileType } from "~/store/profile/types";
 
 type ActivityType = {
   id: string | number;
@@ -11,8 +14,19 @@ type ActivityType = {
 };
 
 export const ProfilePage = () => {
+  const dispatch = useDispatch();
   const profileState = useSelector(getProfile);
   const balanceState = useSelector(getBalance);
+
+  const [form, setForm] = useState({} as ProfileType);
+
+  useEffect(() => {
+    setForm(profileState);
+  }, [profileState]);
+
+  const handleSave = () => {
+    dispatch(profileActions.updateProfile(form));
+  };
 
   return (
     <main className="flex flex-col items-center justify-between min-h-[100vh] bg-white">
@@ -34,99 +48,121 @@ export const ProfilePage = () => {
           <div className="w-full flex flex-row gap-8">
             {/* Profile Contact Section */}
             <div id="profile-contact" className="w-3/5">
-              <Form method="POST">
-                <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-                  {/* Username */}
-                  <div className="sm:col-span-2">
-                    <label htmlFor="username" className="block text-sm font-semibold leading-6 text-gray-900">
-                      User Name
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        id="username"
-                        name="username"
-                        type="text"
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        value={profileState.username ?? ''}
-                      />
-                    </div>
-                  </div>
-
-                  {/* Profile */}
-                  <div className="sm:col-span-2">
-                    <label htmlFor="profile" className="block text-sm font-semibold leading-6 text-gray-900">
-                      Profile
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        id="profile"
-                        name="profile"
-                        type="text"
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        value={profileState.profile ?? ''}
-                      />
-                    </div>
-                  </div>
-
-                  {/* Email */}
-                  <div className="sm:col-span-2">
-                    <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
-                      Email
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        value={profileState.email ?? ''}
-                      />
-                    </div>
-                  </div>
-
-                  {/* Phone */}
-                  <div className="sm:col-span-2">
-                    <label htmlFor="phone" className="block text-sm font-semibold leading-6 text-gray-900">
-                      Phone
-                    </label>
-                    <div className="mt-2.5">
-                      <input
-                        id="phone"
-                        name="phone"
-                        type="text"
-                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        value={profileState.phone ?? ''}
-                      />
-                    </div>
-                  </div>
-
-                  {/* Recent */}
-                  <div className="sm:col-span-2">
-                    <label htmlFor="recent" className="block text-sm font-semibold leading-6 text-gray-900">
-                      Recent Activities
-                    </label>
-                    <div className="mt-2.5 flex flex-col gap-2">
-                      {profileState.activity && profileState.activity.map((item: ActivityType) => {
-                        return (
-                          <ActivityCard
-                            key={crypto.randomUUID()}
-                            {...item}
-                          />
-                        );
-                      })}
-                    </div>
+              <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+                {/* Username */}
+                <div className="sm:col-span-2">
+                  <label htmlFor="username" className="block text-sm font-semibold leading-6 text-gray-900">
+                    User Name
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      id="username"
+                      name="username"
+                      type="text"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      value={form.username ?? ''}
+                      onChange={(evt: any) => {
+                        setForm({
+                          ...form,
+                          username: evt.target.value
+                        });
+                      }}
+                    />
                   </div>
                 </div>
 
-                <div className="mt-10">
-                  <button
-                    type="submit"
-                    className="block w-full rounded-md bg-indigo-950 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Save
-                  </button>
+                {/* Profile */}
+                <div className="sm:col-span-2">
+                  <label htmlFor="profile" className="block text-sm font-semibold leading-6 text-gray-900">
+                    Profile
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      id="profile"
+                      name="profile"
+                      type="text"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      value={form.profile ?? ''}
+                      onChange={(evt: any) => {
+                        setForm({
+                          ...form,
+                          profile: evt.target.value
+                        });
+                      }}
+                    />
+                  </div>
                 </div>
-              </Form>
+
+                {/* Email */}
+                <div className="sm:col-span-2">
+                  <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                    Email
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      value={form.email ?? ''}
+                      onChange={(evt: any) => {
+                        setForm({
+                          ...form,
+                          email: evt.target.value
+                        });
+                      }}
+                    />
+                  </div>
+                </div>
+
+                {/* Phone */}
+                <div className="sm:col-span-2">
+                  <label htmlFor="phone" className="block text-sm font-semibold leading-6 text-gray-900">
+                    Phone
+                  </label>
+                  <div className="mt-2.5">
+                    <input
+                      id="phone"
+                      name="phone"
+                      type="text"
+                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      value={form.phone ?? ''}
+                      onChange={(evt: any) => {
+                        setForm({
+                          ...form,
+                          phone: evt.target.value
+                        });
+                      }}
+                    />
+                  </div>
+                </div>
+
+                {/* Recent */}
+                <div className="sm:col-span-2">
+                  <label htmlFor="recent" className="block text-sm font-semibold leading-6 text-gray-900">
+                    Recent Activities
+                  </label>
+                  <div className="mt-2.5 flex flex-col gap-2">
+                    {profileState.activity && profileState.activity.map((item: ActivityType) => {
+                      return (
+                        <ActivityCard
+                          key={crypto.randomUUID()}
+                          {...item}
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
+
+              <div className="mt-10">
+                <button
+                  className="block w-full rounded-md bg-indigo-950 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  onClick={() => handleSave()}
+                >
+                  Save
+                </button>
+              </div>
             </div>
 
             {/* Profile Ethereum Section */}
